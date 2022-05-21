fb46f5189b7d2c45c68274e2834ca2da833128ec
Akoneh Silas <silasmagho18@gmail.com>
2022-05-21 17:13:15 +0100

    initial

---
+++ index.js
@@ -0,0 +1,53 @@
+
+const input = document.getElementById("search-bar-id");
+const display = document.getElementsByClassName("grid")[0];
+const inputbtn = document.getElementById("formsection");
+console.log(inputbtn);
+inputbtn.addEventListener('submit', submit)
+// inputbtn.addEventListener('click',function(event){
+//     if (event.key == 'Enter')
+//     loadImg();
+// })
+const newInput = document.getElementById("searchbarid");
+console.log(newInput);
+function submit(event){
+    event.preventDefault();
+    
+    console.log(newInput)
+    loadImg();
+}
+function loadImg(){
+    removeImages();
+    const url = 'https://api.unsplash.com/search/photos?query='+newInput.value+'&per_page=9&client_id=SF70ZK36aVoMRCrGlSO6tDxwwNY1fg3oX0Tq3uiTh9o';
+    fetch(url)
+    .then(response => {
+        if(response.ok){
+            return response.json();
+        }else{
+            alert(response.status)
+        }
+    })
+    .then(data =>{
+        const imageNode = [];
+        console.log("wow")
+        console.log(data)
+        for(let i = 0;i < data.results.length; i++){
+            imageNode[i] = document.createElement('div');
+            imageNode[i].className = "img";
+            imageNode[i].style.backgroundImage = 'url('+data.results[i].urls.regular +')';
+            console.log(data.results[i].urls.regular)
+            display.innerHTML += "<div><img src="+ data.results[i].urls.regular +" /></div>"
+            imageNode[i].addEventListener('dblclick',function(){
+                window.open(data.results[i].links.download, '_blank')
+                
+            })
+            console.log("wowo")
+            display.appendChild(imageNode[i]);
+        }console.log(imageNode)
+    }
+
+    )
+}
+function removeImages(){
+    display.innerHTML='';
+}
